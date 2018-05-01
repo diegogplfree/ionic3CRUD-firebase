6194e6645d964b9516dcd4c6aa725fdbb014b69b
Diego Arboleda <diego.gpl.free@gmail.com>
2018-05-01 14:16:39 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+1. Go to src\app\firebase.credentials.ts and put your firebase configuration.
+2. npm install.
+
+To test local:
+- ionic lab
+
+To generate new page with lazy loading:
+- ionic generate page PageName.
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+	apiKey: "YOUR_FIREBASE_API_KEY",
+    authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
+    databaseURL: "YOUR_FIREBASE_DATABASE_URL",
+    projectId: "YOUR_FIREBASE_PROJECT_ID",
+    storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_FIREBASE_SENDER_ID"
+}
